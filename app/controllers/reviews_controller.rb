3777fe1b072a33caa380de99a8d768b54e7e2f70
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-11-11 15:36:44 +0100

    added basic routes, controllers and actions

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,43 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews =  Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+    @review.restaurant_id = @restaurant.id
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destry
+
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: :new_review
+  post "restaurants/:restaurant_id/reviews/", to: "reviews#create", as: :create_review
+  resources :restaurants, :reviews
 end
